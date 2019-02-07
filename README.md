2bb6c183911da24e6ed6977dd8c042e767e5e50a
FOG <fog@ipa.fhg.de>
2019-02-07 12:51:02 +0100

    updated test

--- README.md
+++ README.md
@@ -1,14 +1,34 @@
-# ipa_rescale_img
+# The ipa_rescale_img Package
 Helper Pkg to reduce Bandwidth (e.g. WIFI) for Visualization
 
 Scales down and reduce FPS from camera images.
 
 
-## Example how to save WIFI Bandwidth:
+# Parameter Setting:
 
-Uncompressed Color Image (30 FPS, 640x480)    Bandwidth = 27500 KB/s
+Param:  rescale_factor  is used to resize input image in Percentages
+Param:  reduce_fps  is used to skip  x times before processing next frame
 
-Compressed Color Image (30 FPS, 640x480)    Bandwidth = 750 KB/s
 
-Compressed Color Image & this Pkg (15 FPS, 0.2* 640x480)    Bandwidth = 50 KB/s
+## Example Bandwidth of color images:
 
+Uncompressed Color Image (sensor_msgs::Image, 30 FPS, 640x480)    Bandwidth = 27500 KB/s
+
+Compressed Color Image (sensor_msgs::CompressedImage 30 FPS, 640x480)    Bandwidth = 750 KB/s
+
+
+## Example how to save WIFI Bandwidth by reducing resolution:
+
+1. Compressed Color Image & this Pkg (30 FPS, 320x240)    Bandwidth = 235 KB/s
+
+1. Compressed Color Image & this Pkg (30 FPS, 128x96)    Bandwidth = 75 KB/s
+
+
+## Example how to save WIFI Bandwidth by reducing FPS:
+
+1. Compressed Color Image & this Pkg (15 FPS, 640x480)    Bandwidth = 200 KB/s
+
+
+## Example how to save WIFI Bandwidth by reducing FPS and resolution:
+
+1. Compressed Color Image & this Pkg (15 FPS, 128x96)    Bandwidth = 37 KB/s
--- src/ipa_rescale_img_node.cpp
+++ src/ipa_rescale_img_node.cpp
@@ -20,9 +20,9 @@
 IPARescaleImgNode::IPARescaleImgNode(ros::NodeHandle node_handle):
     node_(node_handle)
 {
-    sub_img_one = node_.subscribe("/camera/rgb/image_raw/compressed", 10, &IPARescaleImgNode::img1Callback, this);
-    sub_img_two = node_.subscribe("/camera/rgb/image_raw/compressed2", 10, &IPARescaleImgNode::img2Callback, this);
-    sub_img_three = node_.subscribe("img_bgr1_out", 10, &IPARescaleImgNode::img2Callback, this);
+    sub_img_one = node_.subscribe("img_bgr1_in", 10, &IPARescaleImgNode::img1Callback, this);
+    sub_img_two = node_.subscribe("img_bgr2_in", 10, &IPARescaleImgNode::img2Callback, this);
+    sub_img_three = node_.subscribe("output1", 10, &IPARescaleImgNode::img3Callback, this);
 
     pub_img_one = node_.advertise<sensor_msgs::CompressedImage>("img_bgr1_out",10);
     pub_img_two = node_.advertise<sensor_msgs::CompressedImage>("img_bgr2_out",10);
@@ -43,43 +43,36 @@ void IPARescaleImgNode::img1Callback(const sensor_msgs::CompressedImageConstPtr&
         ROS_ERROR("Could not convert to image!");
     }
 
+            // get params from launch file
+            double rescale_factor = 1.0;
+            int reduce_fps = 2;
+            ros::param::get("~rescale_factor", rescale_factor);
+            ros::param::get("~reduce_fps", reduce_fps);
 
-             // get params from launch file
-             double rescale_factor = 0.2;
-             int reduce_fps = 2;
-             ros::param::get("~rescale_factor", rescale_factor);
-             ros::param::get("~reduce_fps", reduce_fps);
+            int height = (int)(image.rows * rescale_factor);
+            int width = (int)(image.cols * rescale_factor);
 
-             int height = (int)(image.rows * rescale_factor);
-             int width = (int)(image.cols * rescale_factor);
+            static int counter;
+            if(++counter == reduce_fps) //reduce FPS
+            {
 
+                // resize img
+                cv::Mat img_small;
+                cv::resize(image, img_small, cv::Size(width, height)); //  set factor for resize
+                std::vector<int> params;
+                params.resize(3, 0);
+                params[0] = CV_IMWRITE_JPEG_QUALITY;
+                params[1] = 80; // jpg quality [0-100]
 
+                sensor_msgs::CompressedImage compressed;
+                compressed.header = msg->header;
+                compressed.format = msg->format;
+                cv::imencode(".jpg", img_small, compressed.data, params);
 
+                pub_img_one.publish(compressed);
 
-     	static int counter;
-        if(++counter == reduce_fps) //reduce FPS
-        {
-
-         // resize img
-            cv::Mat img_small;
-            cv::resize(image, img_small, cv::Size(width, height)); //  set factor for resize
-            cv::imshow("img_small", img_small);
-            cv::waitKey(1);
-
-            std::vector<int> params;
-            params.resize(3, 0);
-            params[0] = CV_IMWRITE_JPEG_QUALITY;
-            params[1] = 80; // jpg quality [0-100]
-
-            sensor_msgs::CompressedImage compressed;
-            compressed.header = msg->header;
-            compressed.format = msg->format;
-            cv::imencode(".jpg", img_small, compressed.data, params);
-
-            pub_img_one.publish(compressed);
-
-            counter = 0;
-        }
+                counter = 0;
+            }
 
 }
 
@@ -99,7 +92,7 @@ void IPARescaleImgNode::img2Callback(const sensor_msgs::CompressedImageConstPtr&
 
 
              // get params from launch file
-             double rescale_factor = 10;
+             double rescale_factor = 1.0;
              int reduce_fps = 1;
              ros::param::get("~rescale_factor", rescale_factor);
              ros::param::get("~reduce_fps", reduce_fps);
@@ -117,8 +110,6 @@ void IPARescaleImgNode::img2Callback(const sensor_msgs::CompressedImageConstPtr&
          // resize img
             cv::Mat img_small;
             cv::resize(image, img_small, cv::Size(width, height)); //  set factor for resize
-            cv::imshow("img_small2", img_small);
-            cv::waitKey(1);
 
             std::vector<int> params;
             params.resize(3, 0);
@@ -144,7 +135,7 @@ void IPARescaleImgNode::img3Callback(const sensor_msgs::CompressedImageConstPtr&
 {
 
 
-    // TEST
+    // TEST CB
 
 
 
@@ -161,10 +152,9 @@ void IPARescaleImgNode::img3Callback(const sensor_msgs::CompressedImageConstPtr&
 
 
     // get params from launch file
-    double rescale_factor = 5;
+    double rescale_factor = 10;
     int reduce_fps = 1;
-    ros::param::get("~rescale_factor", rescale_factor);
-    ros::param::get("~reduce_fps", reduce_fps);
+
 
     int height = (int)(image.rows * rescale_factor);
     int width = (int)(image.cols * rescale_factor);
@@ -172,7 +162,7 @@ void IPARescaleImgNode::img3Callback(const sensor_msgs::CompressedImageConstPtr&
     // resize img
     cv::Mat img_small;
     cv::resize(image, img_small, cv::Size(width, height)); //  set factor for resize
-    cv::imshow("img_small2", img_small);
+    cv::imshow("test_img", img_small);
     cv::waitKey(1);
 
 
